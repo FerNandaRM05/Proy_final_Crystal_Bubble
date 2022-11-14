d6cb484d99bbf17c261c7182b6145d6530f39b38
adrian-af <adrian.fernandez.close@gmail.com>
2022-11-14 09:48:02 +0100

    página completa de contacto

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import { Redirect, Route } from 'react-router-dom';
-import {
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
-  setupIonicReact
-} from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle, home, chatbubbles, bagHandle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
+import { IonContent, IonPage, IonTitle, IonToolbar, IonApp, IonHeader, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs, setupIonicReact } from '@ionic/react';
+import logoCB from "./img/icon.png";
+import traducir from "./img/translate.png";
+import "./App.css";
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -39,11 +33,21 @@ setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
+    <IonHeader>
+        <IonToolbar>
+          <IonTitle id='titulo'>
+            <div id='bar'>
+              <img src={logoCB} width={"20%"}></img>
+              <img src={traducir} width={"20%"} id="traducir"></img>
+            </div>
+          </IonTitle>
+        </IonToolbar>
+      </IonHeader>
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
           <Route exact path="/tab1">
-            <Tab1 />
+            <Tab1/>
           </Route>
           <Route exact path="/tab2">
             <Tab2 />
