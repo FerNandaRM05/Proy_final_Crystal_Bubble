27feab0e22b497d62bee940f8cd5f2395ab57eed
KevsCha <k-e-v-i-n_13_@hotmail.com>
2022-11-17 13:29:28 +0100

    Se agrega Slider, modificacion en Themes para dark

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 import { IonHeader, IonIcon, IonImg } from "@ionic/react";
 import { language } from "ionicons/icons";
+import './Header.css';
 
 export const Header:React.FC = () => {
     return(
-        <IonHeader>
-            <IonImg className="img" src="./img/logo.png" />
-            <IonIcon icon={language}/>
+        <IonHeader className="header">
+            <IonImg className="img" src="./img/logo2.png" />
+            <IonImg className="img2" src="./icon/language.svg" />
         </IonHeader>
     );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,106 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonCard,
+  IonCardContent,
+  IonCardTitle,
+  IonContent,
+  IonImg,
+  IonLabel,
+  IonPage,
+  IonButton,
+  IonSlides,
+  IonSlide,
+  IonIcon,
+  IonList,
+} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
-import { Header } from'../components/Header';
+import { playCircleOutline, logoInstagram, logoWhatsapp } from 'ionicons/icons';
+import { Header } from '../components/Header';
 
 const Tab2: React.FC = () => {
+
+  const slideOpts = {
+    initialSlide: 0,
+    speed: 2500,
+    autoplay: true
+  };
+
   return (
     <IonPage>
       <Header />
-      <IonContent fullscreen>
-        <ExploreContainer name="Tab 2 page" />
+      <IonContent fullscreen={true}>
+        <IonSlides pager={true} options={slideOpts}>
+
+          <IonSlide>
+            <IonCardContent className='slider_uno'>
+              <IonCardTitle className='title'>Tu talla en tu estilo</IonCardTitle>
+              
+{/*               <IonList>
+                <IonIcon size="large" icon={playCircleOutline} />
+                <IonIcon size="large" icon={logoInstagram} />
+                <IonIcon size="large" icon={logoWhatsapp} />
+              </IonList> */}
+
+            </IonCardContent>
+          </IonSlide>
+
+          <IonSlide>
+            <IonCardContent className='slider_dos'>
+              <IonCardTitle className='title'>Mas que nada Compromiso</IonCardTitle>
+            </IonCardContent>
+          </IonSlide>
+
+          <IonSlide>
+            <IonCardContent className='slider_tres'> 
+              <IonCardTitle className='title'>Asesorarte en Moda</IonCardTitle>
+            </IonCardContent>
+          </IonSlide>
+            
+          <IonSlide>
+            <IonCardContent className='slider_cuatro'>
+              <IonCardTitle className='title'>Una Caja Muchos Momentos</IonCardTitle>
+            </IonCardContent>
+          </IonSlide>
+
+        </IonSlides>
+
+        {/* <ExploreContainer name="Tab 2 page" /> */}
+        <h1>MODA CRYSTAL BUBBLE TENDENCIAS PRÊT-À-PORTER</h1>
+        <div className='section'>
+          <IonCard className='card1'>
+            <IonImg src='./img/card2.webp' />
+            <IonCardContent >
+              <IonCardTitle>MUJER PETITE</IonCardTitle>
+              <IonLabel>Viste elegante y combina a tu aire</IonLabel>
+            </IonCardContent>
+          </IonCard>
+          <IonCard className='card2'>
+            <IonImg src='./img/card1.webp' />
+            <IonCardContent>
+              <IonCardTitle>MUJER PETITE</IonCardTitle>
+              <IonLabel>Viste elegante y combina a tu aire</IonLabel>
+            </IonCardContent>
+          </IonCard>
+          <IonCard className='card3'>
+            <IonImg src='./img/card3.webp' />
+            <IonCardContent>
+              <IonCardTitle>MUJER PETITE</IonCardTitle>
+              <IonLabel>Viste elegante y combina a tu aire</IonLabel>
+            </IonCardContent>
+          </IonCard>
+        </div>
+        <div className='banner'>
+          <IonCard color={'primary'}>
+            <IonCardContent>
+              <IonCardTitle>
+                TU CÓMPLICE PERFECTA PARA SORPRENDER
+              </IonCardTitle>
+              ¿Sabes cuánto puede cambiar el día de alguien con una sorpresa así?
+            </IonCardContent>
+            <IonButton fill="clear" color={'tertiary'}>Action 1</IonButton>
+          </IonCard>
+        </div>
+
       </IonContent>
     </IonPage>
   );
