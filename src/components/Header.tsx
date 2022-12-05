62bbafdb93c477ecc747ae57a524839fb0b9ca55
Juan Daniel <juancastano0398@gmail.com>
2022-12-06 00:19:40 +0100

    intalacion de librerias

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { IonHeader, IonIcon, IonImg } from "@ionic/react";
+import Switch from "../animations/Switch";
 
 import './Header.css';
 
@@ -6,6 +7,7 @@ export const Header:React.FC = () => {
     return(
         <IonHeader className="header">
             <IonImg className="img" src="./img/logo2.png" />
+            <Switch />
         </IonHeader>
     );
 };
